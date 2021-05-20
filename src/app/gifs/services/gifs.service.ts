16d4bd55c0e0fc36076f056090b4fa8db280f067
ConxiCr <conxicr@hotmail.com>
2021-05-20 11:38:42 +0200

    limitar cantidad de items y controlar duplicados
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,12 +8,19 @@ export class GifsService {
     private _historial:string[] = [];
 
   get historial(){
+
     //Le ponemos un spert para que no se modifique el arreglo original. Se hace rompiendo la referencia
     return [...this._historial];
   }
   //Función para insertar valores al nuevo historial. query=termino de busqueda. unshift para insertar al inicio
   buscarGifs(query:string){
-      this._historial.unshift( query );
+      
+      //como evitar duplicados. Sino lo incluye, sino esta incluido anteriormente, lo insertamos si o si sino existe
+      if( !this._historial.includes( query )){
+        this._historial.unshift( query );
+      }
+      //cuando alguien acceda al historial corta la cantidad máxima de apuntes
+      this._historial = this._historial.splice(0,10);
       console.log(this._historial);
   }
 
